e94784a49175f4ff0da8ce6b2a036410ce196d50
galynary <107271972+galynary@users.noreply.github.com>
2023-05-21 09:09:02 +0300

    change

--- src/components/FollowersItem/FollowersItem.jsx
+++ src/components/FollowersItem/FollowersItem.jsx
@@ -1,6 +1,7 @@
 import { List, Item, DeleteButton } from './ContactList.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { addFollowers } from 'redux/fellowers/operations';
 import { selectFollowers } from 'redux/fellowers/selectors';
 import { Loader } from 'components/Loader/Loader';
 import { User, Wrapper } from './User.styled';
@@ -9,14 +10,13 @@ export const FollowersItem = ({ data }) => {
   const { id, userURL, tweets, followers } = data;
   const dispatch = useDispatch();
   const follower = useSelector(selectFollowers);
-  initialState.data = {
-	followers:100,500.
-  };
 
   useEffect(() => {
     dispatch(fetchFellowers());
   }, [dispatch]);
-  const handlerAddFollowers = () => {};
+  const handlerAddFollowers = () => {
+    dispatch(addFollowers(id));
+  };
 
   return (
     <Wrapper>
@@ -24,12 +24,9 @@ export const FollowersItem = ({ data }) => {
         <User alt="user" src={userURL} width={80} />
         <Text>TWEETS{tweets}</Text>
         <Text>FOLLOWERS{followers}</Text>
-        <AddFellowersButton
-          type="button"
-          onClick={() => dispatch(handlerAddFollowers(id))}
-        >
-          FOLLOW
-        </AddFellowersButton>
+        <Button type="button" onClick={handlerAddFollowers}>
+          {following ? 'FOLLOWING' : 'FOLLOW'}
+        </Button>
       </Item>
     </Wrapper>
   );
